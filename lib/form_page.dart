466e2324a6d87e543e1e4967c1b86769becdeecd
Miguel_Cassar <mcassar@onpointserv.com>
2025-04-02 17:55:55 +0200

    api working and returning list. need to add button for new list and better ui

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -1,6 +1,32 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
+class Place {
+  final String name;
+  final double? rating;
+  final String? googleMapsUri;
+  final String? priceRange;
+
+  Place({
+    required this.name,
+    this.rating,
+    this.googleMapsUri,
+    this.priceRange
+  });
+
+  // Updated Factory Method
+  factory Place.fromJson(Map<String, dynamic> json) {
+    return Place(
+      name: json['displayName']?['text'] ?? "Unknown", // Get 'text' from nested map
+      rating: json['rating']?.toDouble(),
+      googleMapsUri: json['googleMapsUri'],
+      priceRange: json['priceRange'],
+    );
+  }
+}
 
 class FormPage extends StatefulWidget {
   
@@ -13,11 +39,15 @@ class FormPage extends StatefulWidget {
 class _FormPageState extends State<FormPage> {
   String locationMessage = "Location not available";
   String selectedRadius = '250';
-  String selectedPlaceType = 'pharmacies';
+  String selectedPlaceType = 'pharmacy';
   Position? currentPosition;
+
+
+
 
   Future<void> _getCurrentLocation() async {
     bool serviceEnabled;
+
     LocationPermission permission;
     // Check if location services are enabled
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
@@ -54,14 +84,55 @@ class _FormPageState extends State<FormPage> {
     });
   }
 
-  void _submitForm() async {
+  Future<List<Place>> _submitForm() async {
+    const apiKey = 'AIzaSyBOdHxzQvUP8hT40j9ny3TTaTM3dMzlmA0';
+    final url = Uri.parse(
+      'https://places.googleapis.com/v1/places:searchNearby?key=$apiKey');
     await _getCurrentLocation();
     if (currentPosition != null) {
-      print('Radius: $selectedRadius');
-      print('Place Type: $selectedPlaceType');
-      print('Location: ${currentPosition!.latitude}, ${currentPosition!.longitude}');
-    } else {
+          // JSON body for POST request
+      final requestBody = jsonEncode({
+        "includedTypes": [selectedPlaceType],
+        "maxResultCount": 10,
+        "locationRestriction": {
+          "circle": {
+            "center": {"latitude": currentPosition!.latitude, "longitude": currentPosition!.longitude},
+            "radius": int.parse(selectedRadius)
+          }
+        }
+      });
+
+      // Send POST request
+      try {
+        final response = await http.post(
+          url,
+          headers: {
+            "Content-Type": "application/json",
+            "X-Goog-FieldMask":
+                "places.displayName,places.rating,places.googleMapsUri,places.priceRange",
+          },
+          body: requestBody,
+        );
+
+        if (response.statusCode == 200) {
+          final data = jsonDecode(response.body);
+
+          List<Place> places = data['places']
+              .map<Place>((json) => Place.fromJson(json))
+              .toList();
+
+          return places;
+        } else {
+          print("Error: ${response.statusCode}");
+          return [];
+        }
+      } catch (e) {
+        print("Exception occurred: $e");
+        return [];
+      }
+    }else {
       print('Location not available');
+      return [];
     }
   }
 
@@ -77,7 +148,7 @@ class _FormPageState extends State<FormPage> {
             DropdownButton<String>(
               value: selectedRadius,
               onChanged: (value) => setState(() => selectedRadius = value!),
-              items: ['250', '500', '1000'].map((value) {
+              items: ['250', '500', '1000', '2000'].map((value) {
                 return DropdownMenuItem(value: value, child: Text('$value meters'));
               }).toList(),
             ),
@@ -85,13 +156,16 @@ class _FormPageState extends State<FormPage> {
             DropdownButton<String>(
               value: selectedPlaceType,
               onChanged: (value) => setState(() => selectedPlaceType = value!),
-              items: ['pharmacies', 'supermarkets', 'grocery stores'].map((value) {
+              items: ['pharmacy', 'supermarket', 'grocery_store', 'bus_stop', 'restaurant', 'atm', 'bank', 'hotel *'].map((value) {
                 return DropdownMenuItem(value: value, child: Text(value));
               }).toList(),
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _submitForm,
+              onPressed: () async{
+                List<Place> places = await _submitForm();
+                Navigator.pop(context, places); // Pass the list of places back to the previous screen
+              },
               child: Text('Search'),
             ),
           ],
--- lib/list_page.dart
+++ lib/list_page.dart
@@ -1,22 +1,66 @@
+import 'package:find_the_way/form_page.dart';
 import 'package:flutter/material.dart';
-import 'form_page.dart';
 
-class ListPage extends StatelessWidget {
+class ListPage extends StatefulWidget {
+  final List<Place>? places; // Accept list of places
+
+  ListPage({this.places}); // Constructor to receive data
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  List<Place> nearbyPlaces = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // If data is passed, populate the list
+    if (widget.places != null) {
+      nearbyPlaces = widget.places!;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Nearby Places')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => FormPage()),
-            );
-          },
-          child: Text('Go to Search Form'),
-        ),
-      ),
+      body: nearbyPlaces.isEmpty
+          ? Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FormPage()),
+                  ).then((result) {
+                    if (result != null) {
+                      setState(() {
+                        nearbyPlaces = result;
+                      });
+                    }
+                  });
+                },
+                child: Text('Go to Search Form'),
+              ),
+            )
+          : ListView.builder(
+              itemCount: nearbyPlaces.length,
+              itemBuilder: (context, index) {
+                final place = nearbyPlaces[index];
+                return ListTile(
+                  title: Text(place.name),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (place.rating != null) Text("Rating: ${place.rating}"),
+                      if (place.googleMapsUri != null) Text("Google Maps: ${place.googleMapsUri}"),
+                      if (place.priceRange != null) Text("Price Range: ${place.priceRange}")
+                    ],
+                  ),
+                );
+              },
+            ),
     );
   }
 }
